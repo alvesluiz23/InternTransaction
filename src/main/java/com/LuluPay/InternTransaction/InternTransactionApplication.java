957e1d4375bddef570875f6ba6bbb0f6351f7867
Luiz <alvesluizeduardo23@gmail.com>
2026-02-13 18:15:16 -0300

    chore: initial project structure from spring initializr

---
+++ src/main/java/com/LuluPay/InternTransaction/InternTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.LuluPay.InternTransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LuluPay/InternTransaction/InternTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LuluPay.InternTransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
